9639e1735ed8e6b28c675be1d0b91ca7de88e760
goswamijaykumar <jaygoswami1121@gmail.com>
2025-01-08 19:10:52 -0500

    Added final configuration file

--- ODriveVersion/configuration.py
+++ ODriveVersion/configuration.py
@@ -35,7 +35,7 @@ class CoreConfigurationValues():
     # Power Configuration
     MAX_POSITIVE_DC_CURRENT = 6
     MAX_NEGATIVE_DC_CURRENT = -6
-    MAX_DC_OVERVOLTAGE_TRIP_LEVEL = 24
+    MAX_DC_OVERVOLTAGE_TRIP_LEVEL = 26
     MIN_DC_OVERVOLTAGE_TRIP_LEVEL = 12
 
 def setControllerConfiguration(odrv):
@@ -55,7 +55,7 @@ def setCoreConfiguration(odrv):
     odrv.axis0.config.motor.motor_type = config.MOTOR_TYPE
     odrv.axis0.config.motor.pole_pairs = config.MOTOR_POLE_PAIRS
     odrv.axis0.config.motor.torque_constant = config.MOTOR_TORQUE_CONSTANT
-    odrv.axis0.config.motor.current_soft_max = config.MOTOR_CURRENT_SOFT_MAX
+    odrv.axis0.config.motor.current_soft_max = config.MOTOR_CURRENT_SOFT_MAX 
     odrv.axis0.config.motor.calibration_current = config.MOTOR_CALIBRATION_CURRENT
     odrv.config.dc_bus_overvoltage_trip_level = config.MAX_DC_OVERVOLTAGE_TRIP_LEVEL
     odrv.config.dc_bus_undervoltage_trip_level = config.MIN_DC_OVERVOLTAGE_TRIP_LEVEL
@@ -65,13 +65,36 @@ def setCoreConfiguration(odrv):
     odrv.axis0.config.commutation_encoder = config.MOTOR_ENCODER
     odrv.rs485_encoder_group0.config.mode = config.MOTOR_ENCODER_MODEL
     
-    #odrv.save_configuration()
-    #odrv.reboot()
+def fullCalibration(odrv):
+    odrv.axis0.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
+    time.sleep(15)
 
-def isOdriveConfigurationValid():
+def isOdriveConfigurationValid(odrv):
     '''Validate ODrive configurable variable match with tested values in config file'''
+    config = CoreConfigurationValues()
+    if (odrv.axis0.config.motor.motor_type == config.MOTOR_TYPE and
+        odrv.axis0.config.motor.pole_pairs == config.MOTOR_POLE_PAIRS and
+        odrv.axis0.config.motor.current_soft_max == config.MOTOR_CURRENT_SOFT_MAX and
+        odrv.config.dc_bus_overvoltage_trip_level == config.MAX_DC_OVERVOLTAGE_TRIP_LEVEL and
+        odrv.config.dc_bus_undervoltage_trip_level == config.MIN_DC_OVERVOLTAGE_TRIP_LEVEL and
+        odrv.config.dc_max_positive_current == config.MAX_POSITIVE_DC_CURRENT and
+        odrv.config.dc_max_negative_current == config.MAX_NEGATIVE_DC_CURRENT and
+        odrv.axis0.config.load_encoder == config.MOTOR_ENCODER and
+        odrv.axis0.config.commutation_encoder == config.MOTOR_ENCODER and
+        odrv.rs485_encoder_group0.config.mode == config.MOTOR_ENCODER_MODEL):
+        print("Core Config are correct!")   
+    else:
+        print("Core Config are not correct!")
+    
+    check_torque_constent = odrv.axis0.config.motor.torque_constant
+    check_calibration_current = odrv.axis0.config.motor.calibration_current
+    print(check_torque_constent) 
+    print(check_calibration_current)
     False
 
 if __name__ == "__main__":
     odrv = odrive.find_any()
     setCoreConfiguration(odrv)
+    setControllerConfiguration(odrv)  
+    fullCalibration(odrv)
+    isOdriveConfigurationValid(odrv)
